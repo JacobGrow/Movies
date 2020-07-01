116b936466e6e50991f437eef581214afc14af50
Jacob Grow <jacobagrow@gmail.com>
2020-07-01 14:51:13 -0600

    Changed some styling

--- clientsrc/src/views/Home.vue
+++ clientsrc/src/views/Home.vue
@@ -1,65 +1,63 @@
 <template>
-  <div class="home">
+  <div class="home text-light">
+    <div class="container-fluid">
     <div class="row">
-      <div class="col text-center">
-    <h1>Movies</h1>
+      <div class="col-12 text-center">
+        <h1>Movies</h1>
       </div>
     </div>
     <form @submit.prevent="searchMovies">
-<input type="text" v-model="searchApi">
-<button type="submit" @click="searchResults = true">Search</button>
+      <input type="text" v-model="searchApi" />
+      <button type="submit" @click="searchResults = true">Search</button>
     </form>
-    <div>
+    <h2>
       Results
-      <div v-for="movie in movies" :key="movie.id">
-        <ul>
-         <li> {{movie.Title}} 
-            <img :src="movie.Poster"/> </li> 
-        </ul>
+    </h2>
+      <div class="row justify-content-center">
+        <div v-for="movie in movies" :key="movie.id">
+          <div class="col-4 text-center">
+            <p>{{movie.Title}}</p>
+          
+              <img :src="movie.Poster"/>  
+          
+        </div>
       </div>
-    </div>
+          </div>
    
-<div class="row">
+    </div>
 
-<!-- <popularMovies /> -->
-</div>
-<div>
- 
-</div>
+    <div class="row">
+      <!-- <popularMovies /> -->
+    </div>
+    <div></div>
   </div>
 </template>
 
 
 <script>
-
 export default {
   name: "home",
   data() {
     return {
       searchResults: false,
       searchApi: ""
-    }
+    };
   },
-  mounted() {
-  
-  },
+  mounted() {},
   computed: {
-    movies(){
-      return this.$store.state.searchResults
+    movies() {
+      return this.$store.state.searchResults;
     }
   },
   methods: {
-    async searchMovies(){
+    async searchMovies() {
       this.results = [];
       this.$store.dispatch("searchMovies", this.searchApi);
       this.results = this.$store.state.searchResults;
     }
   },
-  components: {
-   
-    }
-  
-}
+  components: {}
+};
 </script>
 
 
