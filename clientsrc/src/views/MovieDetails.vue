c8c3f1b4eaef6f52c3ab84819757ae3e1c713441
Jacob Grow <jacobagrow@gmail.com>
2020-07-01 23:05:20 -0600

    Styling to Details Page

--- clientsrc/src/views/MovieDetails.vue
+++ clientsrc/src/views/MovieDetails.vue
@@ -2,11 +2,16 @@
   <div class="movieDetails container-fluid">
     <div class="row">
       <div class="col">
-        <h1>Active Movie</h1>
- <h5>
- {{ movie.Title }}
- </h5>
-<img :src="movie.Poster" class="movie-size align-self-center"/>  
+        <div class="row justify-content-center">
+          <h1 class="text-light">{{ movie.Title }}</h1>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-3">
+            <div class="card card-color mt-2 p-2 text-center shadow">
+              <img :src="movie.Poster" class="movie-size align-self-center" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -16,18 +21,21 @@
 export default {
   name: "movieDetails",
   props: ["movieProp"],
-  mounted(){
-    this.$store.dispatch("getActiveMovie", this.$route.params.movieId)
+  mounted() {
+    this.$store.dispatch("getActiveMovie", this.$route.params.movieId);
   },
   computed: {
     movie() {
       return this.$store.state.activeMovie;
-      console.log(this.activeMovie)
+      console.log(this.activeMovie);
     }
   },
-  methods: {
-    
-  }
-
-}
+  methods: {}
+};
 </script>
+
+<style scoped>
+.card-color {
+  background-color: #b3211c;
+}
+</style>
